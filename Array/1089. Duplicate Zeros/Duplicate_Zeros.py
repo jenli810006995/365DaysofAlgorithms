820201d1a9defc76d7145c45fad342eea73385eb
Jen <38231830+jenli810006995@users.noreply.github.com>
2020-12-23 20:22:00 -0500

    Create Duplicate_Zeros.py

---
+++ Array/1089. Duplicate Zeros/Duplicate_Zeros.py
@@ -0,0 +1,30 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        possible_dups = 0
+        length_ = len(arr) - 1
+        
+        for left in range(length_ + 1):
+            if left > length_ - possible_dups: ## will exceed the boundary
+                break
+                
+            if arr[left] == 0:
+                if left == length_ - possible_dups:
+                    arr[length_] = 0
+                    length_ -= 1
+                    break
+                possible_dups += 1
+                
+        last = length_ - possible_dups
+        
+        for i in range(last, -1, -1): ## range(start, stop, step) takes three arguments.
+            if arr[i] == 0:
+                arr[i + possible_dups] = 0
+                possible_dups -= 1
+                arr[i + possible_dups] = 0
+            else:
+                arr[i + possible_dups] = arr[i]
+                
+ ## Link: https://leetcode.com/problems/duplicate-zeros/
+            
+                
+                
