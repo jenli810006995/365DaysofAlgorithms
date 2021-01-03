27973ecfedf769b4829e3d0e24466083d15df3e7
Jen <38231830+jenli810006995@users.noreply.github.com>
2021-01-03 08:02:25 -0500

    Create Word_Search.java

---
+++ Backtracking/79. Word Search/Word_Search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int i = 0; i < board.length; i ++)
+            for(int j = 0; j < board[0].length; j ++)
+                if(board[i][j] == word.charAt(0) && isFound(board,i,j,word,0)) // isFound is built below
+                    return true;
+        return false;
+    }
+        
+    private boolean isFound(char[][] board, int i, int j, String word, int index){
+        if (index == word.length()) return true; // it is in the boundary
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length) return false;
+        if(word.charAt(index) != board[i][j]) return false;
+        char temp = board[i][j];
+        board[i][j] = '*';
+        if(isFound(board, i + 1, j, word, index + 1) ||
+           isFound(board, i - 1, j, word, index + 1) || 
+           isFound(board, i, j + 1, word, index + 1) ||
+           isFound(board, i, j - 1, word, index + 1))
+           return true;
+        board[i][j] = temp;
+        return false;
+    }
+}
+
+// Link: https://leetcode.com/problems/word-search/
