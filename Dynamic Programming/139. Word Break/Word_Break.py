ce41a9949ad4a3720c424aa30ec56a4af39dc87b
Jen <38231830+jenli810006995@users.noreply.github.com>
2021-01-09 08:59:07 -0500

    Create Word_Break.py

---
+++ Dynamic Programming/139. Word Break/Word_Break.py
@@ -0,0 +1,12 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        for i in range(len(s)):
+            if dp[i]:
+                for j in range(1, len(s) + 1):
+                    if s[i:j] in wordDict:
+                        dp[j] = True
+       return dp[-1]
+       
+# Link: https://leetcode.com/problems/word-break/
